9660b8b420be14b8969585941bd59f89020e0775
Chandara-Sin <cdsin68@gmail.com>
2022-12-18 23:20:06 +0700

    style: design cart item

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -20,8 +20,26 @@ class CartPage extends StatelessWidget {
                 Expanded(
                   child: ListView.builder(
                     itemCount: value.cartItems.length,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text(value.cartItems[index][0]),
+                    padding: const EdgeInsets.all(12.0),
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.grey.shade200),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
+                          ),
+                          title: Text(value.cartItems[index][0]),
+                          subtitle: Text("\$${value.cartItems[index][1]}"),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.remove),
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 )
