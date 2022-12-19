6405373f1c824a649ed1a4d9e4d163990c4b1317
Chandara-Sin <cdsin68@gmail.com>
2022-12-19 21:44:35 +0700

    feat: add cart page with items total price

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_shop_app/models/cart_model.dart';
 import 'package:provider/provider.dart';
 
@@ -9,14 +10,22 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "My Cart",
-          style: TextStyle(fontSize: 24),
-        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: Consumer<CartModel>(
         builder: ((context, value, child) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    "My Cart",
+                    style: GoogleFonts.notoSerif(
+                        fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                ),
                 Expanded(
                   child: ListView.builder(
                     itemCount: value.cartItems.length,
@@ -40,6 +49,59 @@ class CartPage extends StatelessWidget {
                           ),
                         ),
                       ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(36.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(8)),
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Total Price",
+                              style: TextStyle(
+                                  color: Colors.green.shade100, fontSize: 16),
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              "\$${value.getTotalPrice()}",
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.green.shade100),
+                              borderRadius: BorderRadius.circular(12)),
+                          padding: const EdgeInsets.all(12),
+                          child: Row(
+                            children: const [
+                              Text(
+                                "Pay Now",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 )
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,66 +13,69 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          const SizedBox(
-            height: 48,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Good Morning,",
-              style: TextStyle(fontSize: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 48,
             ),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Let's order fresh items for you",
-              style: GoogleFonts.notoSerif(
-                  fontSize: 36, fontWeight: FontWeight.bold),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Good Morning,",
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Divider(
-              thickness: 2,
+            const SizedBox(
+              height: 4,
             ),
-          ),
-          const SizedBox(height: 24),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Fresh Items",
-              style: TextStyle(fontSize: 22),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Let's order fresh items for you",
+                style: GoogleFonts.notoSerif(
+                    fontSize: 36, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Expanded(
-            child: Consumer<CartModel>(
-              builder: ((context, value, child) => GridView.builder(
-                    itemCount: value.shopItems.length,
-                    padding: const EdgeInsets.all(12),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2, childAspectRatio: 1 / 1.3),
-                    itemBuilder: ((context, index) => GroceryItemTile(
-                          itemName: value.shopItems[index][0],
-                          itemPrice: value.shopItems[index][1],
-                          imgPath: value.shopItems[index][2],
-                          color: value.shopItems[index][3],
-                          onPressed: () =>
-                              Provider.of<CartModel>(context, listen: false)
-                                  .addItemToCart(index),
-                        )),
-                  )),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ]),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(
+                thickness: 2,
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Fresh Items",
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+            Expanded(
+              child: Consumer<CartModel>(
+                builder: ((context, value, child) => GridView.builder(
+                      itemCount: value.shopItems.length,
+                      padding: const EdgeInsets.all(12),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, childAspectRatio: 1 / 1.3),
+                      itemBuilder: ((context, index) => GroceryItemTile(
+                            itemName: value.shopItems[index][0],
+                            itemPrice: value.shopItems[index][1],
+                            imgPath: value.shopItems[index][2],
+                            color: value.shopItems[index][3],
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .addItemToCart(index),
+                          )),
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
